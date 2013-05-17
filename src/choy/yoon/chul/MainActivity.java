2420c511c1076ef4da2836dc75f9cff8972adf2d
sapphiredev <sapphire@Sapphire-ui-MacBook-Air.local>
2013-05-17 23:27:59 +0900

    added texturepicker
    added texturepicker

--- src/choy/yoon/chul/MainActivity.java
+++ src/choy/yoon/chul/MainActivity.java
@@ -1,32 +1,38 @@
 package choy.yoon.chul;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
-import android.widget.Toast;
 import choy.yoon.chul.State.EditState;
 import choy.yoon.chul.State.PaintStateManager;
 import choy.yoon.chul.State.PaintStateManager.StateType;
 
-public class MainActivity extends Activity implements ColorPickerDialog.OnColorChangedListener {
+public class MainActivity extends Activity implements
+		ColorPickerDialog.OnColorChangedListener {
 	private int selectedColor;
 	private Bitmap selectedTexture;
 	private PaintView paintView;
-	
+
+	private static final int ACTIVITY_SELECT_IMAGE = 1000;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		PaintStateManager.GetInstance().SetContext(this);
 		paintView = new PaintView(this);
 		setContentView(paintView);
-		
-		selectedTexture = BitmapFactory.decodeResource(getResources(), R.drawable.sample);
+
+		selectedTexture = BitmapFactory.decodeResource(getResources(),
+				R.drawable.sample);
 	}
 
 	@Override
@@ -53,13 +59,16 @@ public class MainActivity extends Activity implements ColorPickerDialog.OnColorC
 	}
 
 	public void onFill(MenuItem item) {
-		switch(item.getItemId()) {
+		switch (item.getItemId()) {
 		case R.id.action_fill_color:
-			Toast.makeText(this, "Color", Toast.LENGTH_SHORT).show();
 			new ColorPickerDialog(this, this, selectedColor).show();
 			break;
 		case R.id.action_fill_texture:
-			Toast.makeText(this, "Texture", Toast.LENGTH_SHORT).show();
+			Intent intent = new Intent(
+					Intent.ACTION_PICK,
+					android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+			startActivityForResult(intent, ACTIVITY_SELECT_IMAGE);
+
 			break;
 		}
 	}
@@ -67,12 +76,41 @@ public class MainActivity extends Activity implements ColorPickerDialog.OnColorC
 	@Override
 	public void colorChanged(int color) {
 		selectedColor = color;
-		EditState state = (EditState)PaintStateManager.GetInstance().GetState(StateType.kStateEdit);
-		if(state.GetShape() != null) {
+		EditState state = (EditState) PaintStateManager.GetInstance().GetState(
+				StateType.kStateEdit);
+		if (state.GetShape() != null) {
 			state.GetShape().SetColor(GLESHelper.GetARGB(color));
 		}
 	}
-	
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		switch (requestCode) {
+		case ACTIVITY_SELECT_IMAGE:
+			if (resultCode == RESULT_OK) {
+				try {
+					Uri selectedImage = data.getData();
+					String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+					Cursor cursor = getContentResolver().query(selectedImage,
+							filePathColumn, null, null, null);
+					cursor.moveToFirst();
+
+					int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+					String filePath = cursor.getString(columnIndex);
+					cursor.close();
+
+					textureChanged(BitmapFactory.decodeFile(filePath));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+			break;
+		}
+	}
+
 	public void textureChanged(Bitmap texture) {
 		selectedTexture = texture;
 	}
